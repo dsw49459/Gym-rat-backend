a4d74af714adf29f654bfd4f4c108621d9c01f88
krzysztoffarys00 <krzysztof.farys@grupablix.pl>
2025-04-25 23:39:05 +0200

    fix

--- src/data/MongoClientProvider.kt
+++ src/data/MongoClientProvider.kt
@@ -4,9 +4,16 @@ import org.litote.kmongo.coroutine.*
 import org.litote.kmongo.reactivestreams.KMongo
 
 object MongoClientProvider {
-    private val connectionString = System.getenv("MONGODB_URI")
-        ?: "mongodb+srv://ysiu16:YOUR_PASSWORD@gymrat.fotkcyz.mongodb.net/?retryWrites=true&w=majority&appName=GymRat"
+    private val connectionString: String by lazy {
+        System.getenv("MONGODB_URI") ?:
+        "mongodb+srv://ysiu16:IS3z1BGLDsga46AE@gymrat.fotkcyz.mongodb.net/?retryWrites=true&w=majority&appName=GymRat"
+    }
 
-    val client: CoroutineClient = KMongo.createClient(connectionString).coroutine
-    val database: CoroutineDatabase = client.getDatabase("GymRat")
+    val client: CoroutineClient by lazy {
+        KMongo.createClient(connectionString).coroutine
+    }
+
+    val database: CoroutineDatabase by lazy {
+        client.getDatabase("GymRat")
+    }
 }
