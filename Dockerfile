ee907213e12bbb68c19f7a7ce50cfbd2ce19c40f
krzysztoffarys00 <krzysztof.farys@grupablix.pl>
2025-04-25 22:51:00 +0200

    docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Używamy JDK 17 slim
+FROM openjdk:17-jdk-slim
+
+# Ustawiamy katalog roboczy
+WORKDIR /app
+
+# Kopiujemy pliki projektu
+COPY . .
+
+# Budujemy aplikację
+RUN ./gradlew build
+
+# Ustawiamy port, który aplikacja będzie nasłuchiwać
+EXPOSE 8080
+
+# Uruchamiamy aplikację z katalogu /release
+CMD ["java", "-jar", "release/app.jar"]
--- src/data/MongoClientProvider.kt
+++ src/data/MongoClientProvider.kt
@@ -4,9 +4,9 @@ import org.litote.kmongo.coroutine.*
 import org.litote.kmongo.reactivestreams.KMongo
 
 object MongoClientProvider {
-    private val connectionString = System.getenv("MONGO_URI")
+    private val connectionString = System.getenv("MONGODB_URI")
         ?: "mongodb+srv://ysiu16:YOUR_PASSWORD@gymrat.fotkcyz.mongodb.net/?retryWrites=true&w=majority&appName=GymRat"
 
     val client: CoroutineClient = KMongo.createClient(connectionString).coroutine
-    val database: CoroutineDatabase = client.getDatabase("GymRat") // Nazwa Twojej bazy na Atlasie
+    val database: CoroutineDatabase = client.getDatabase("GymRat")
 }
